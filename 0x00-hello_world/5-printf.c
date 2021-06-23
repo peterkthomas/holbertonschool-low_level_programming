04f9eab8822c85ab3fc6c3baa85cae6b45593759
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-23 09:35:40 -0400

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
