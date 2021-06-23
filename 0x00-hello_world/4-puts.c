defbd99a3386dc11ea7720547e8b4f0f9345228e
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-23 09:33:49 -0400

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char txt_string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(txt_string);
+
+	return (0);
+}
