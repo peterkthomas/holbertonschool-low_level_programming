c9ba4809c47e1678ad02cd90fecb4a373e945237
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-23 09:46:02 -0400

    Added advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
