ce8ff8e8ede61cd04c817d0fb959561f0883266d
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-25 15:40:55 -0400

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,12 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j <= 99; j++) /* j is always 1 higher */
+		/**
+		 * j is always 1 higher
+		 * 0 0 1 1 -> but because j/10 reduces to tens column
+		 * we get 00 01, 00 02...
+		 */
+		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0'); /* tens */
 			putchar((i % 10) + '0'); /* ones */
