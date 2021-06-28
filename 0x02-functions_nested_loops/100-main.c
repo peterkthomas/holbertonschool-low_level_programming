d04538129bc8340517cfbf1c10182af98b0592aa
Peter Thomas <peter@peterkthomas.com>
2021-06-28 16:59:10 -0700

    added _putchar.c and 100-main.c

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_times_table(13);
+    _putchar('\n');
+    return (0);
+}
