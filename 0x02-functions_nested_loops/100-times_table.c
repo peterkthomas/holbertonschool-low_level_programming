03119aaee38daaa6f81f8a19897f6385c88baa32
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 16:24:19 -0400

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,32 +6,33 @@
  */
 void print_times_table(int n)
 {
-	int i;
-	int j;
-	int result;
+	int i, j, result;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < 10; j++)
+	if(n <= 15 && n >= 0)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
-			int result_tens = result / 10;
-			int result_ones = result % 10;
-			if (result < 10)
-				_putchar('result_ones');
-			else
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(result_tens + '0');
-				_putchar(result_ones + '0');
-			}
+				result = i * j;
+				int result_hundreds = result / 100;
+				int result_tens = (result / 10) % 10;
+				int result_ones = (result % 100) % 10;
+				
+				if (result < 10)
+					_putchar('result_ones');
+				else
+				{
+					_putchar(result_tens + '0');
+					_putchar(result_ones + '0');
+				}
 
-			if (j < 9)
-			{
-				_putchar(',');
-				_putchar(' ');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+					_putchar('\n');
 			}
-			else
-				_putchar('\n');
 		}
-	}
 }
