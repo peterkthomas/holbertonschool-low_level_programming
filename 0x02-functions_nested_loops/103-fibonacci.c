8f21d29ee67230a1727abd7d81d3fce1d784c3ee
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 17:42:51 -0400

    Create 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int fib_one = 1; /* fib number 1 */
+	unsigned long int fib_two = 2; /* fib number 2 */
+	unsigned long int next = fib_one + fib_two; /* result of 1+2 */
+
+	unsigned long total = 2; /* start with our first even*/
+
+	while (fib_one + fib_two < 4000000)
+	{
+		fib_one = fib_two;
+		fib_two = next;
+		next = fib_one + fib_two;
+		
+		if (next % 2 == 0)
+			total += next;
+	}
+
+	printf("%ul \n", total); /* print the number */
+
+	return (0);
+}
