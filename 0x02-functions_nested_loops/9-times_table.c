d33589b99a30a70426af254fd1ad69a9a67979d5
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 12:52:06 -0400

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,22 @@ void times_table(void)
 		_putchar('0'); /* start with a 0 */
 		_putchar(',');
 		_putchar(' ');
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			result = i * j;
 			result_tens = result / 10;
 			result_ones = result % 10;
 
-			if (result && result < 10)
+			if (!result)
+			{
+				_putchar('0');
+			}
+			else if (result && result < 10)
 			{
 				_putchar(' ');
 				_putchar(result_ones + '0');
 			}
-			else if (result)
+			else
 			{
 				_putchar(result_tens + '0');
 				_putchar(result_ones + '0');	
