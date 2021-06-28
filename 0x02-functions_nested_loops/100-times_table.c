1c9f7c62a9c8143fdc99e6aecb5d6b4c1ba41c19
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 16:41:38 -0400

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,6 +9,7 @@ void print_times_table(int n)
 	int i, j, result;
 
 	if (n <= 15 && n >= 0)
+	{
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
@@ -42,5 +43,6 @@ void print_times_table(int n)
 				
 			}
 		}
-		_putchar('\n');
+	_putchar('\n');
+	}
 }
