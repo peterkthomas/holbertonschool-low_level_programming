e77ffc7cd33b0f2642ed4d5c1509106412cdd0c7
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 12:43:40 -0400

    fixed zero condition print

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 			if (!result)
 			{
 				_putchar('0');
+				_putchar('0');
 			}
 			else if (result < 10)
 			{
@@ -31,7 +32,7 @@ void times_table(void)
 			else
 			{
 				_putchar(result_tens + '0');
-				_putchar(result_ones + '0');
+				_putchar(result_ones + '0');	
 			}
 
 			if (j < 9)
