97bea37a680fdf7ae6b179a8e40c52ca442db216
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 12:29:23 -0400

    fixed tens and ones placement

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,21 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int i;
-	int j;
-
-	for (i = 0; i < 24; i++)
+	for (int i = 0; i < 24; i++)
 	{
 		int hour_tens = i / 10;
 		int hour_ones = i % 10;
 
-		for (j = 0; j < 60; j++)
+		for (int j = 0; j < 60; j++)
 		{
 			int minute_tens = j / 10;
 			int minute_ones = j % 10;
 
-			_putchar(hour_ones + '0');
 			_putchar(hour_tens + '0');
+			_putchar(hour_ones + '0');
 			_putchar(':');
 			_putchar(minute_tens + '0');
 			_putchar(minute_ones + '0');
