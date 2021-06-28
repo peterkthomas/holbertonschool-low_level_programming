1bcfcd0199ecd70c1591514627ee625e79977d4a
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 17:27:30 -0400

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int fib_one = 1; /* fib number 1 */
+	int fib_two = 2; /* fib number 2 */
+	int next = fib_one + fib_two; /* result of 1+2 */
+
+	printf("%d, %d, ", fib_one, fib_two);
+	for (i = 3; i < 50; ++i)
+	{
+		printf("%d, ", next);
+		fib_one = fib_two;
+		fib_two = next;
+		next = fib_one + fib_two;
+	}
+
+	printf("\n"); /* print the number */
+
+	return (0);
+}
