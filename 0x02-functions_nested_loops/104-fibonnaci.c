c6421d39eafc91f9dfd8d1fb455528a8fbc67fff
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-06-28 17:46:20 -0400

    just throwing first test at checker

---
+++ 0x02-functions_nested_loops/104-fibonnaci.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int fib_one = 1; /* fib number 1 */
+	unsigned long int fib_two = 2; /* fib number 2 */
+	unsigned long int next = fib_one + fib_two; /* result of 1+2 */
+
+	printf("%lu, %lu, ", fib_one, fib_two);
+	for (i = 2; i < 100; ++i)
+	{
+		printf("%lu", next);
+		fib_one = fib_two;
+		fib_two = next;
+		next = fib_one + fib_two;
+		if (i < 99)
+			printf(", ");
+	}
+
+	printf("\n"); /* print the number */
+
+	return (0);
+}
