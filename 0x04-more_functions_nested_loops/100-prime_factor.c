aae4657a2a2e15d9b6cae55522a25f4ff479365f
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-01 13:47:50 -0400

    prime factor problem

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+    int i = 2;
+    int x = 2;
+    unsigned long num = 612852475143;
+
+    while (num != 1)
+    {
+        while (num % i == 0)
+        {
+            num = num / i;
+            x = i;
+        }
+        i++;
+    }
+    printf("%d\n", x);
+    return (0);
+}
