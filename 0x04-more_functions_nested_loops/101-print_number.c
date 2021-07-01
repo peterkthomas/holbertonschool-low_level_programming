383292af0bbf96fee2b4db82cbcb6cfb393b6b76
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-01 15:54:46 -0400

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,7 +10,7 @@ void print_number(int n)
 		_putchar('-');
 		n *= -1;
 	}
-	if (n / 10 > 0) /* we have another column to add */
+	if (n / 10) /* we have another column to add */
 		print_number(n / 10); /* recursion call to print the next column number */
 	_putchar((n % 10) + '0'); /* finally, print the ones. */
 }
