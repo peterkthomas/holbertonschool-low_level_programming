cb4216be9d76dce3f25ca761aae70c401be739f8
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-01 10:27:09 -0400

    Created 0x04 directory, readme, first project

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks if character is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if c is upper, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return 1;
+	return 0;
+}
