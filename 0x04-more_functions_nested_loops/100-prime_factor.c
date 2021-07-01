e1c50f7c895f899777465c2249cf90e7c1eb7df9
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-01 13:51:30 -0400

    fixed linting

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - program entry
- * 
- * Return: 0
- */
+* main - program entry
+*
+* Return: 0
+*/
 int main(void)
 {
-    int i = 2;
-    int x = 2;
-    unsigned long num = 612852475143;
+	int i = 2;
+	int x = 2;
+	unsigned long num = 612852475143;
 
-    while (num != 1)
-    {
-        while (num % i == 0)
-        {
-            num = num / i;
-            x = i;
-        }
-        i++;
-    }
-    printf("%d\n", x);
-    return (0);
+	while (num != 1)
+	{
+		while (num % i == 0)
+		{
+			num = num / i;
+			x = i;
+		}
+		i++;
+	}
+	printf("%d\n", x);
+	return (0);
 }
