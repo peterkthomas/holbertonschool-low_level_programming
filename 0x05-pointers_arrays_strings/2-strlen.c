4e5cc3a4ba08410e3e9b5dcd236bee8b56a5e6e9
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-02 10:16:39 -0400

    strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strlen - calculates length of string
+ * @s: pointer to string
+ * 
+ * Return: length of string (int)
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != 0) /* loop through char array by pointer */
+	{
+		s++;
+		length++;
+	}
+
+	return length;
+}
