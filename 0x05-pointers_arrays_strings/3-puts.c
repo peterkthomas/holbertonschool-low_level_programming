9f102f2a209a991be41a1f6141c8a996371160b5
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-02 10:23:08 -0400

    _puts function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts - writes an array of chars
+ * @str: pointer to string
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != 0) /* loop through char array by pointer */
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 void swap_int(int *a, int *b);
+void _puts(char *str);
