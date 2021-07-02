9f079c2f8dcfedfd7b24eb095a0e1488ee0ef620
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-02 12:30:26 -0400

    9-strcpy added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * strcpy - copies one string buffer to another
+ * @dest: destination string buffer
+ * @src: source string buffer
+ *
+ * Return: pointer to dest buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i]) /* copy the whole string */
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
