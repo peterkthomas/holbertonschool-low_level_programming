c2a5a7a525af48193c61b8377c9b65f9e57462cc
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-02 11:14:39 -0400

    rev string function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,20 @@
 void rev_string(char *s)
 {
 	int i = 0;
+	int j = 0;
+	char hold;
 
 	while (s[i]) /* loop through char array by pointer */
 		i++;
 
-	while (i >= 0)
+	i -= 1; /* i is now length of string */
+
+	while (j < i) /* loop until meet string length */
 	{
-		putchar(s[i]);
-		i--;
+		hold = s[j]; /* s[0] is now held */
+		s[j] = s[i]; /* s[0] is now last char */
+		s[i] = hold; /* last char is now first char */
+		j++; /* increment to s[1] */
+		i--; /* decrement to s[len-1] */
 	}
-	putchar('\n');
 }
