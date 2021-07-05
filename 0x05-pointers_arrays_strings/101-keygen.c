b4d292ed313f4a7041883b32f9f09a1c2f261ecf
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-05 10:01:38 -0400

    removed useless loop

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,7 +4,6 @@
 
 int main(void)
 {
-	
 	int i;
 	int key;
 	char *str_rand = NULL;
@@ -26,15 +25,12 @@ int main(void)
 
 		str_rand[length] = '\0';
 
-		for (i = 0; i < length;i++)
-		{
-			val = str_rand[0] + str_rand[1] + str_rand[2] + str_rand[3] + str_rand[4] + str_rand[5];
-			val += str_rand[6] + str_rand[7] + str_rand [ 8] + str_rand [9] + str_rand[10] + str_rand[11];
-			val += str_rand[12] + str_rand[13] + str_rand[14] + str_rand[15] + str_rand[16] + str_rand[17];
-			val += str_rand[18] + str_rand[19] + str_rand[20] + str_rand[21] + str_rand[22] + str_rand[23];
-			val += str_rand[24] + str_rand[25] + str_rand[26]+ str_rand[27] + str_rand[28] + str_rand[29];
+		val = str_rand[0] + str_rand[1] + str_rand[2] + str_rand[3] + str_rand[4] + str_rand[5];
+		val += str_rand[6] + str_rand[7] + str_rand [ 8] + str_rand [9] + str_rand[10] + str_rand[11];
+		val += str_rand[12] + str_rand[13] + str_rand[14] + str_rand[15] + str_rand[16] + str_rand[17];
+		val += str_rand[18] + str_rand[19] + str_rand[20] + str_rand[21] + str_rand[22] + str_rand[23];
+		val += str_rand[24] + str_rand[25] + str_rand[26]+ str_rand[27] + str_rand[28] + str_rand[29];
 
-		}
 
 	}
 
