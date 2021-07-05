a3a74038e55b1a8864de64844a1bb69c8fb74b36
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-04 21:51:26 -0400

    i can't believe I solved this.. it took all day. lol

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	srand(time(0));
+	char range[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789,.-#'?!";
+	char *str_rand = NULL;
+	int length = 29;
+	int i;
+	int key;
+
+	long val = 0;
+
+	while (val != 2772) 
+	{
+		str_rand = malloc(sizeof(char) * length - 1);
+
+		for (i = 0; i < length; i++)
+		{
+			key = rand() % 68;
+			str_rand[i] = range[key];
+		}
+
+		str_rand[length] = '\0';
+
+		for( i = 0; i < length;i++)
+		{
+			val = str_rand[0] + str_rand[1] + str_rand[2] + str_rand[3] + str_rand[4] + str_rand[5];
+			val += str_rand[6] + str_rand[7] + str_rand [ 8] + str_rand [9] + str_rand[10] + str_rand[11];
+			val += str_rand[12] + str_rand[13] + str_rand[14] + str_rand[15] + str_rand[16] + str_rand[17];
+			val += str_rand[18] + str_rand[19] + str_rand[20] + str_rand[21] + str_rand[22] + str_rand[23];
+			val += str_rand[24] + str_rand[25] + str_rand[26]+ str_rand[27] + str_rand[28] + str_rand[29];
+
+		}
+
+	}
+
+	printf("%s", str_rand);
+
+	return (0);
+
+}
