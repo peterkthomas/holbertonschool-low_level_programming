f9096efe0b7f70b8df0666e0964dc4794124c60d
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-09 12:49:38 -0400

    pointer updates

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *ptr = dest;
+	char *ptr_dest = dest;
+	char *ptr_src = src;
 
 	for (i = 0; i < n; i++)
-		src[i] = dest[i];
+		ptr_src[i] = ptr_dest[i];
 
-	return (ptr);
+	return (dest);
 }
