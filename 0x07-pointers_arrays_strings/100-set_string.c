842a8f256fb5300b0c4299d0cbea3515471d1982
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-09 13:20:56 -0400

    set string project

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value ptr to a char
+ * @s: pointer to be set
+ * @to: char to point to
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
