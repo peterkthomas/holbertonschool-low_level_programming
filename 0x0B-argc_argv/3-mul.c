1402d616a64ed845ad9cfde3a2700bff8ec2469b
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-16 09:49:30 -0400

    Create 3-mul.c

---
+++ 0x0B-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry
+ * @argc: argument count
+ * @argv: argument variables
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	if (argc == 3)
+	{
+		x = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", x);
+		return (0);
+	}
+
+	return (1);
+}
