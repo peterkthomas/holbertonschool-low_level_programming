6c491508c58e4c86c858dcf7c38c1e6f12908c80
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-16 09:32:20 -0400

    Create 1-args.c

---
+++ 0x0B-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - program entry
+ * @argc - argument count
+ *
+ * Return: 0
+ */
+int main(int argc)
+{
+	if(argc)
+		printf("%d\n", argc);
+
+	return (0);
+}
