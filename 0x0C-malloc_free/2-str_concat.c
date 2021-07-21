6d4feaf6c426083e2a2fee41cd35b1db795ede76
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-20 22:24:45 -0400

    Fixed null checks

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	int j = 0;
 	int length = 0;
 
-	if (s1 == NULL || s2 == NULL)
-	{
+	if (!s1)
 		s1 = "";
+
+	if (!s2)
 		s2 = "";
-	}
 
 	for (i = 0; s1[i] || s2[i]; i++)
 		length++;
