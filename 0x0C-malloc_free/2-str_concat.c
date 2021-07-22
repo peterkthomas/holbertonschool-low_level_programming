95a66b938b37f69ea71fde6317823a81da74946b
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-07-22 10:28:16 -0400

    Update 2-str_concat.c

--- 0x0C-malloc_free/2-str_concat.c
+++ 0x0C-malloc_free/2-str_concat.c
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		length++;
 
-	new_str = malloc(sizeof(char) * length + 1);
+	new_str = malloc(sizeof(char) * length);
 
 	if (!new_str)
 	{
