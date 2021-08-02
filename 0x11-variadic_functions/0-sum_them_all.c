baa85037c789ab37d6456f375580804355249a92
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-02 17:13:28 -0400

    Update 0-sum_them_all.c

--- 0x11-variadic_functions/0-sum_them_all.c
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include <stdarg.h>
+#include "variadic_functions.h"
 
 /**
  * sum_them_all - sum of all ar guments
@@ -9,14 +10,14 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int total;
+	int total = 0;
 	va_list list;
 
-	if (!n)
+	if (n == 0)
 		return (n);
 
 	va_start(list, n);
-	for (i = 0, total = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		total += va_arg(list, int);
 
 	va_end(list);
