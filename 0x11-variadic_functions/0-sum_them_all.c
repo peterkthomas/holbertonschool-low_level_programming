f5b60146b7cbf507d887acce93c41da599f15092
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-02 16:54:36 -0400

    initial commit

---
+++ 0x11-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum of all ar guments
+ * @n: number of parameters
+ *
+ * Return: Sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int total;
+	va_list list;
+
+	va_start(list, n);
+	for (i = 0, sum = 0; i < n, i++)
+		sum+= va_arg(list, int);
+
+	va_end(list);
+
+	return(total);
+}
