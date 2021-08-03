f330d5d9a71e2044b7b3de69a2827fb9b24df62e
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-03 15:13:04 -0400

    Update dog.h

--- 0x0F-structures_typedef/dog.h
+++ 0x0F-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef dog
 #define dog
 
+/**
+ * struct dog - dog
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 Struct Dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
