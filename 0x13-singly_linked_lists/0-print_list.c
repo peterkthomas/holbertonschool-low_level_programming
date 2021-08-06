6a50f7e82821b2451b39f62611a89285fcc40899
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-06 12:41:39 -0400

    checker fix attempt 1

--- 0x13-singly_linked_lists/0-print_list.c
+++ 0x13-singly_linked_lists/0-print_list.c
@@ -9,15 +9,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
+	const list_t *tmp_list = h;
 
-	while (h)
+	while (tmp_list)
 	{
-		if (h->str == NULL)
+		if (!tmp_list->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%d] %s\n", tmp_list->len, tmp_list->str);
 		nodes++;
-		h = h->next;
+		tmp_list = tmp_list->next;
 	}
 
 	return (nodes);
