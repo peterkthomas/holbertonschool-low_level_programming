b96235a20673303e36013d1c0afec9f3581b72be
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-11 18:45:28 -0400

    initial upload

---
+++ 0x14-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint list
+ * @h: pointer to a listint
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
---
+++ 0x14-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+int _putchar(char c);
+#endif
+
