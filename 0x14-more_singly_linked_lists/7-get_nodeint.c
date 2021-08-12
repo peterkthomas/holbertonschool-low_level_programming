fef55fe1d17a1f36d64292da47ee1e64afe52ec1
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-11 22:52:29 -0400

    Update 7-get_nodeint.c

--- 0x14-more_singly_linked_lists/7-get_nodeint.c
+++ 0x14-more_singly_linked_lists/7-get_nodeint.c
@@ -11,10 +11,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (!*head)
+	if (!head)
 		return (0);
 
-	while (list)
+	while (head)
 	{
 		if (i == index)
 			return (head);
