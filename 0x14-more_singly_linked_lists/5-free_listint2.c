a57ffa57c20f3fec4243bde4f18c6fb214c75cd3
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-11 22:13:13 -0400

    project 5

---
+++ 0x14-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to head of list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *list;
+
+
+	while (head && head->next)
+	{
+		list = head->next;
+		free(&head->n);
+		head = list;
+	}
+
+	free(head);
+	*head = NULL;
+}
