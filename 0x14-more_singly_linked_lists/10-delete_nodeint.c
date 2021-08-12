4b72cf76e12d107c1638da316229c538b5ed3680
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-11 23:35:51 -0400

    updated while block for extra null check

--- 0x14-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x14-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 
-	while (local_index < index)
+	while (list && (local_index < index))
 	{
 		list = list->next;
 		local_index++;
