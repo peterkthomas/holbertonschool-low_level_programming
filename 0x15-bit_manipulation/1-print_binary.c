a6885aef41cd467adcbc1e170181aafe4472965e
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-12 23:29:20 -0400

    removed unused variable, fixed return value

--- 0x15-bit_manipulation/1-print_binary.c
+++ 0x15-bit_manipulation/1-print_binary.c
@@ -3,22 +3,19 @@
 /**
  * print_binary - prints a number in binary
  * @n: number to convert
- *
  */
 
 void print_binary(unsigned long int n)
 {
-	char c;
-
 	if (!n)
 	{
 		_putchar('0');
-		return (0);
+		return;
 	}
 	if (n == 1)
 	{
 		_putchar('1');
-		return (0);
+		return;
 	}
 
 	print_binary (n >> 1);
