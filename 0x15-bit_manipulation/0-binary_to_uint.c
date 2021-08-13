a36866d96dcbc11fbeff3b8fb38d3f51bf56b20c
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-12 23:58:05 -0400

    Fixed second while loop calc

--- 0x15-bit_manipulation/0-binary_to_uint.c
+++ 0x15-bit_manipulation/0-binary_to_uint.c
@@ -16,20 +16,20 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 
-	while (b[i] != '\0')
+	while (b[i])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
 
-	i = 0;
+	i--;
 
-	while (b[i] != '\0')
+	while (i >= 0)
 	{
 		if (b[i] == '1')
 			n += power;
-		i++;
+		i--;
 		power *= 2;
 	}
 
