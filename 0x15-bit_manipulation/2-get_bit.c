6a431680e91a58fdba8224090ac144a97178ad53
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-12 23:34:51 -0400

    fixed variable name conflict (had two n's)

--- 0x15-bit_manipulation/2-get_bit.c
+++ 0x15-bit_manipulation/2-get_bit.c
@@ -10,15 +10,15 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int n = 0;
+	unsigned int x = 0;
 
 	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	while (n < index)
+	while (x < index)
 	{
 		n = n >> 1;
-		n++;
+		x++;
 	}
 
 	return ((n & 1));
