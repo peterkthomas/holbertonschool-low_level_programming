26353e109f7d36cc15e3f398d44ae41b967dc5e3
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:35:05 -0400

    linting

--- 0x16-file_io/2-append_text_to_file.c
+++ 0x16-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if(!text_content)
+	if (!text_content)
 		text_content = "";
 
 	fd = open(filename, O_RDWR | O_APPEND);
