9e1e65162eb3c88486cc9abbdc5350bfe9d0a75e
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:14:25 -0400

    Fixed variable name mixup

--- 0x16-file_io/0-read_textfile.c
+++ 0x16-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	fd = open(filename, O_RDONLY);
 
-	if (file == -1)
+	if (fd == -1)
 	{
 		free(buffer);
 		return (0);
--- 0x16-file_io/holberton.h
+++ 0x16-file_io/holberton.h
@@ -3,7 +3,13 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
