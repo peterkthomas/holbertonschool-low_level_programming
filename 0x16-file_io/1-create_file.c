447cbbb3a2347573421f53536d63979162acf31e
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:23:37 -0400

    Update 1-create_file.c

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int status;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
