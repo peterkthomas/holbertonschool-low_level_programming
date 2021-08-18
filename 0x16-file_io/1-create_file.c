352f2b0f2d19ed17db7a432809019d8120462d62
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:27:04 -0400

    changed is not check from 0 to -1

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -12,12 +12,12 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int status;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
-	if (!fd)
+	if (fd == -1)
 		return (-1);
 
 	status = write(fd, text_content, strlen(text_content));
