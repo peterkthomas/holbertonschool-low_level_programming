50eafab96e324be7270240cee6c8a9f293f1769a
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:28:02 -0400

    made empty text content into empty string

--- 0x16-file_io/1-create_file.c
+++ 0x16-file_io/1-create_file.c
@@ -12,8 +12,11 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	int status;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
+
+	if (!text_content)
+		text_content = "";
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
