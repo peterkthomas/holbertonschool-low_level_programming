40522111f98993fb47652e46d4dab42d7cd41eab
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-08-17 21:22:26 -0400

    Project 1 files

---
+++ 0x16-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of file to create (pointer)
+ * @text_content: string to write to file (pointer)
+ *
+ * Return: -1 if fail, 1 if success
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int status;
+
+	if (!filename || !text_content)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (!fd)
+		return (-1);
+
+	status = write(fd, text_content, strlen(text_content));
+
+	if (status == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
