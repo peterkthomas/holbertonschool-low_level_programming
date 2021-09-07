4698b24ef825c4ad80a204655923d5a218a6007b
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-07 15:28:26 -0400

    Project 1 commit

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Lists number of dlistint_t nodes 
+ * @nodes: number of nodes
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
