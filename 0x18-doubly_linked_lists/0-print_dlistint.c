a0c516e19d40dd2b6ac934f4221fb6e0c91a29b4
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-07 15:03:52 -0400

    Commit of README, header, project 0

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a dlistint_t
+ * @h: pointer to the list
+ *
+ * Return: size_t of the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes;
+
+	while (h)
+	{
+		printf("%d\n", h->);
+		h = h->next;
+		nodes++;
+	}
+
+	return (n);
+}
