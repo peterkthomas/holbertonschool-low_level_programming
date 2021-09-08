6d22f56e4e735fd5e853b45159c7556fcc213a59
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-08 17:02:56 -0400

    project 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * _calloc - allocate memory for an array of elements
+ *				all size bytes long, all at 0
+ * @num: number of elements
+ * @size: size in bytes
+ *
+ * Return: pointer to allocated memory, or NULL if fail
+ */
+void *_calloc(size_t nmemb, size_t size)
+{
+	char *ptr;
+	size_t i;
+
+	if (!nmemb)
+		return (NULL);
+
+	if (!size)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
