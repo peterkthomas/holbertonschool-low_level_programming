6a95eaef1d35341e58d1f927e07328b61ac220f6
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-08 16:48:09 -0400

    Added project 1

---
+++ 0x0E-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * string_nconcat - concatenates two strings of at most n bytes
+ * @s1: first string.
+ * @s2: second string.
+ * @n: Max bytes in string 2 to add to string 1
+ *
+ * Return: null on fail, or pointer to the concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	int i;
+	int length = n;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	for (i = 0; s1[i]; i++)
+		length++;
+	str = malloc(sizeof(char) * (length + 1));
+
+	if (!str)
+		return (NULL);
+	length = 0;
+
+	for (i = 0; s1[i]; i++)
+		str[length++] = s1[i];
+
+	for (i = 0; i < n && s2[i]; i++)
+		str[length++] = s2[i];
+
+	str[length] = '\0';
+
+	return (str);
+}
