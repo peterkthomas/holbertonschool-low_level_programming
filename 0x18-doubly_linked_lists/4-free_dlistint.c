15a209605930d5ee79a46ed610be311c634477d9
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-08 15:11:33 -0400

    Update 4-free_dlistint.c

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -8,13 +8,11 @@ void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp_node = NULL;
 
-	if (!head)
+
+	while (head)
 	{
-		while (head)
-		{
-			temp_node = head;
-			head = (head)->next;
-			free(temp_node);
-		}
+		temp_node = head;
+		head = (head)->next;
+		free(temp_node);
 	}
 }
