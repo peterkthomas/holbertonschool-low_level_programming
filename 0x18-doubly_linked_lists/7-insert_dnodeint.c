6baacd25045fe5a38518c5a0d6b538818f99bd6a
peterkthomas <80075893+peterkthomas@users.noreply.github.com>
2021-09-08 15:34:55 -0400

    extra space removed

--- 0x18-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x18-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		idx--;
 	}
 
-	if (!temp_node->next )
+	if (!temp_node->next)
 		return (add_dnodeint_end(h, n));
 
 	new_node = malloc(sizeof(dlistint_t));
